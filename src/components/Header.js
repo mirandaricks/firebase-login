02279df962dd330200f2996a9d1b3afc22b7bedc
Rick Miranda <dev.rickmiranda@gmail.com>
2023-05-30 14:19:54 -0300

    update app.js and create component TabelaHome

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = ({isAuthenticated}) => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        {isAuthenticated && (
+          <li>
+            <Link to="/cadastrar">Cadastrar</Link>
+          </li>
+        )}
+        {!isAuthenticated && (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <button className="btnLink" onClick={(event) => {}}>
+              Logout
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
