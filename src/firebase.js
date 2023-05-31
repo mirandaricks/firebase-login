93b535112a037305b5d38852c921032539641d2a
Rick Miranda <dev.rickmiranda@gmail.com>
2023-05-31 07:55:48 -0300

    firebase.js update

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/auth"
 import "firebase/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhip6cAuzau705ZbxX-nHzcDHo3_sqoIw",
-  authDomain: "autheticationfirebase-cb238.firebaseapp.com",
-  projectId: "autheticationfirebase-cb238",
-  storageBucket: "autheticationfirebase-cb238.appspot.com",
-  messagingSenderId: "474355636259",
-  appId: "1:474355636259:web:129b75a4a690c7a38a3996",
-  measurementId: "G-0Q3P19Y250",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
